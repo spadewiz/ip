49092d0c81d83f7d4fac57aac2616fbf2f430d89
spadewiz <panneer869@gmail.com>
2026-08-28 04:17:20 +0800

    Complete Level-1

--- src/main/java/Wiz.java
+++ src/main/java/Wiz.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Wiz {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Wiz.");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
