e70dee78fac9451f53a373891825e3019b83e991
spadewiz <panneer869@gmail.com>
2026-08-28 04:20:50 +0800

    Complete Level-2

--- src/main/java/Wiz.java
+++ src/main/java/Wiz.java
@@ -4,6 +4,9 @@ public class Wiz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello! I'm Wiz.");
         System.out.println("What can I do for you?");
 
@@ -13,9 +16,15 @@ public class Wiz {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
-
-            System.out.println(input);
         }
 
         scanner.close();
