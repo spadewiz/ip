b1442d6ea2db40394095e4fa86e4d799dc39be9a
spadewiz <panneer869@gmail.com>
2026-08-28 04:11:27 +0800

    Complete Level-0

---
+++ src/main/java/Wiz.java
@@ -0,0 +1,7 @@
+public class Wiz {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Wiz.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
